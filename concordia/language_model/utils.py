f783c81d77c576af389f76b478a54d9ba2c97c35
Tyler Hamilton <tyler.hamilton4814@outlook.com>
2025-08-07 21:52:51 +0000

    update language_models

--- concordia/language_model/utils.py
+++ concordia/language_model/utils.py
@@ -1,17 +1,3 @@
-# Copyright 2023 DeepMind Technologies Limited.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     https://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 """Utilities for loading language models."""
 
 from concordia.language_model import amazon_bedrock_model
@@ -23,6 +9,8 @@ from concordia.language_model import language_model
 from concordia.language_model import mistral_model
 from concordia.language_model import no_language_model
 from concordia.language_model import ollama_model
+from concordia.language_model import \
+    openrouter_model  # Added OpenRouter import
 from concordia.language_model import pytorch_gemma_model
 from concordia.language_model import together_ai
 
@@ -32,6 +20,9 @@ def language_model_setup(
     api_type: str,
     model_name: str,
     api_key: str | None = None,
+    base_url: str | None = None,
+    http_referer: str | None = None,
+    x_title: str | None = None,
     device: str | None = None,
     disable_language_model: bool = False,
 ) -> language_model.LanguageModel:
@@ -41,10 +32,11 @@ def language_model_setup(
     api_type: The type of API to use.
     model_name: The name of the specific model to use.
     api_key: The API key to use (if supported).
+    base_url: The base URL for the API (if supported).
+    http_referer: The HTTP referer for OpenRouter (if supported).
+    x_title: The X-Title for OpenRouter (if supported).
     device: The device to use for model processing (if supported).
     disable_language_model: If True then disable the language model.
-      This uses a model that returns an empty string whenever asked for a free
-      text response and a randome option when asked for a choice.
 
   Returns:
     The wrapped language model.
@@ -52,31 +44,38 @@ def language_model_setup(
   if disable_language_model:
     return no_language_model.NoLanguageModel()
 
+  # A dictionary to hold the arguments for the model class
   kwargs = {'model_name': model_name}
-  if api_key is not None:
-    kwargs['api_key'] = api_key
-  if device is not None:
-    kwargs['device'] = device
 
-  if api_type == 'amazon_bedrock':
-    cls = amazon_bedrock_model.AmazonBedrockLanguageModel
-  elif api_type == 'google_aistudio_model':
-    cls = google_aistudio_model.GoogleAIStudioLanguageModel
-  elif api_type == 'google_cloud_custom_model':
-    cls = google_cloud_custom_model.VertexAI
-  elif api_type == 'langchain_ollama':
-    cls = langchain_ollama_model.LangchainOllamaLanguageModel
-  elif api_type == 'mistral':
-    cls = mistral_model.MistralLanguageModel
-  elif api_type == 'ollama':
-    cls = ollama_model.OllamaLanguageModel
-  elif api_type == 'openai':
-    cls = gpt_model.GptLanguageModel
-  elif api_type == 'pytorch_gemma':
-    cls = pytorch_gemma_model.PyTorchGemmaLanguageModel
-  elif api_type == 'together_ai':
-    cls = together_ai.Gemma2
-  else:
+  # A mapping from api_type to the corresponding model class
+  api_to_class = {
+      'amazon_bedrock': amazon_bedrock_model.AmazonBedrockLanguageModel,
+      'google_aistudio_model': google_aistudio_model.GoogleAIStudioLanguageModel,
+      'google_cloud_custom_model': google_cloud_custom_model.VertexAI,
+      'langchain_ollama': langchain_ollama_model.LangchainOllamaLanguageModel,
+      'mistral': mistral_model.MistralLanguageModel,
+      'ollama': ollama_model.OllamaLanguageModel,
+      'openai': gpt_model.GptLanguageModel,
+      'openrouter': openrouter_model.OpenRouterLanguageModel,  # Added OpenRouter
+      'pytorch_gemma': pytorch_gemma_model.PyTorchGemmaLanguageModel,
+      'together_ai': together_ai.Gemma2,
+  }
+
+  if api_type not in api_to_class:
     raise ValueError(f'Unrecognized api type: {api_type}')
 
-  return cls(**kwargs)  # pytype: disable=wrong-keyword-args
+  cls = api_to_class[api_type]
+
+  # Add arguments based on the selected API type
+  if api_key is not None:
+    kwargs['api_key'] = api_key
+  if device is not None and api_type in ['pytorch_gemma']:
+    kwargs['device'] = device
+  if base_url is not None and api_type == 'openrouter':
+      kwargs['base_url'] = base_url
+  if http_referer is not None and api_type == 'openrouter':
+      kwargs['http_referer'] = http_referer
+  if x_title is not None and api_type == 'openrouter':
+      kwargs['x_title'] = x_title
+
+  return cls(**kwargs)
